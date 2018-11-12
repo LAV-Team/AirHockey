f68743ba021a263d45aa0402c0813287103bdf60
Liza Shcherbakova <scherbakova.liz@yandex.ru>
2018-11-12 21:54:45 +0300

    Create MainMenu

--- Classes/AppDelegate/AppDelegate.cpp
+++ Classes/AppDelegate/AppDelegate.cpp
@@ -44,16 +44,31 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+#define FULL_SCREEN_MODE 1
+#if (!FULL_SCREEN_MODE)
+#define FRAME_SIZE_NAME resolutionSizeNo1
+#endif
+#define RELATIVE_SIZE_MODE 0 // IS_DPI_ENABLED
+
+static cocos2d::Size designResolutionSize = cocos2d::Size(640, 360);
+
+#if (!FULL_SCREEN_MODE || !RELATIVE_SIZE_MODE)
+static cocos2d::Size resolutionSizeNo1 = cocos2d::Size(640, 360);
+static cocos2d::Size resolutionSizeNo2 = cocos2d::Size(960, 540);
+static cocos2d::Size resolutionSizeNo3 = cocos2d::Size(1440, 810);
+static cocos2d::Size resolutionSizeNo4 = cocos2d::Size(1920, 1080);
+static cocos2d::Size resolutionSizeNo5 = cocos2d::Size(2880, 1620);
+static cocos2d::Size resolutionSizeNo6 = cocos2d::Size(3840, 2160);
+static cocos2d::Size resolutionSizeNo7 = cocos2d::Size(4800, 2700);
+static cocos2d::Size resolutionSizeNo8 = cocos2d::Size(5760, 3240);
+static cocos2d::Size resolutionSizeNo9 = cocos2d::Size(7680, 4320);
+#endif
 
 AppDelegate::AppDelegate()
 {
 }
 
-AppDelegate::~AppDelegate() 
+AppDelegate::~AppDelegate()
 {
 #if USE_AUDIO_ENGINE
     AudioEngine::end();
@@ -85,7 +100,11 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("AirHockey", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+#if (FULL_SCREEN_MODE)
+        glview = GLViewImpl::createWithFullScreen("AirHockey");
+#else
+        glview = GLViewImpl::createWithRect("AirHockey", cocos2d::Rect(0, 0, FRAME_SIZE_NAME.width, FRAME_SIZE_NAME.height));
+#endif
 #else
         glview = GLViewImpl::create("AirHockey");
 #endif
@@ -98,30 +117,46 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
 
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    // Set design resolution
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::EXACT_FIT);
+
+#if (!RELATIVE_SIZE_MODE)
+    // Set correct scale
     auto frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
+    if (frameSize.height > resolutionSizeNo8.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo9.height / designResolutionSize.height, resolutionSizeNo9.width / designResolutionSize.width));
     }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
+    else if (frameSize.height > resolutionSizeNo7.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo8.height / designResolutionSize.height, resolutionSizeNo8.width / designResolutionSize.width));
     }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
+    else if (frameSize.height > resolutionSizeNo6.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo7.height / designResolutionSize.height, resolutionSizeNo7.width / designResolutionSize.width));
     }
+    else if (frameSize.height > resolutionSizeNo5.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo6.height / designResolutionSize.height, resolutionSizeNo6.width / designResolutionSize.width));
+    }
+    else if (frameSize.height > resolutionSizeNo4.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo5.height / designResolutionSize.height, resolutionSizeNo5.width / designResolutionSize.width));
+    }
+    else if (frameSize.height > resolutionSizeNo3.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo4.height / designResolutionSize.height, resolutionSizeNo4.width / designResolutionSize.width));
+    }
+    else if (frameSize.height > resolutionSizeNo2.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo3.height / designResolutionSize.height, resolutionSizeNo3.width / designResolutionSize.width));
+    }
+    else if (frameSize.height > resolutionSizeNo1.height) {
+        director->setContentScaleFactor(MIN(resolutionSizeNo2.height / designResolutionSize.height, resolutionSizeNo2.width / designResolutionSize.width));
+    }
+    else {
+        director->setContentScaleFactor(MIN(resolutionSizeNo1.height / designResolutionSize.height, resolutionSizeNo1.width / designResolutionSize.width));
+    }
+#endif
 
     register_all_packages();
-    
+
     // create a scene. it's an autorelease object
     auto scene = MenuViewController::createScene();
-    
+
     // run
     director->runWithScene(scene);
 
